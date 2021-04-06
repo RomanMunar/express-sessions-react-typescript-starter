5612016f99b3eee0cd5b550cc9545b43b4b46878
romanmunar <roman.munar1@gmail.com>
2021-04-07 07:49:56 +0800

    added app and main server

---
+++ src/index.ts
@@ -0,0 +1,23 @@
+import mongoose from 'mongoose'
+import session from 'express-session'
+import connectRedis from 'connect-redis'
+import Redis from 'ioredis'
+import { MONGO_URI, MONGO_OPTIONS, APP_PORT } from './config'
+import { createApp } from './app'
+
+const main = async () => {
+  await mongoose.connect(MONGO_URI, MONGO_OPTIONS)
+
+  const RedisStore = connectRedis(session)
+
+  // const client = new Redis(REDIS_OPTIONS) from config/cache
+  const client = new Redis()
+
+  const store = new RedisStore({ client })
+
+  const app = createApp(store)
+
+  app.listen(APP_PORT, () => console.log(`http://localhost:${APP_PORT}`))
+}
+
+main().catch(error => console.error(error))
