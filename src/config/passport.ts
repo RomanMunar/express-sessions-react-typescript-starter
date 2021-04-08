651c68ce8c88ad0494516c5fb1ec6a58b0504c5c
romanmunar <roman.munar1@gmail.com>
2021-04-08 18:10:00 +0800

    added passport strategy env

---
+++ src/config/passport.ts
@@ -0,0 +1,6 @@
+export const {
+  GOOGLE_OAUTH_CLIENT_ID = '913921497487-nd4otaa1dsmlnh62od1ksggqfsifsmvv.apps.googleusercontent.com',
+  GOOGLE_OAUTH_CLIENT_SECRET = 'N20Zy4VBetLVqOvlTnPwiPUx',
+  GITHUB_OAUTH_CLIENT_ID = '87335eb42b2f0496b93f',
+  GITHUB_OAUTH_CLIENT_SECRET = 'd234e78a3b340419c3e617edbff1702a6b32fca4',
+} = process.env
--- src/env.d.ts
+++ src/env.d.ts
@@ -1,21 +1,25 @@
 declare namespace NodeJS {
   interface ProcessEnv {
-    NODE_ENV: string;
-    APP_PORT: string;
-    APP_HOSTNAME: string;
-    APP_PROTOCOL: string;
-    APP_SECRET: string;
-    MONGO_USERNAME: string;
-    MONGO_PASSWORD: string;
-    MONGO_HOST: string;
-    MONGO_PORT: string;
-    MONGO_DATABASE: string;
-    REDIS_PORT: string;
-    REDIS_HOST: string;
-    REDIS_PASSWORD: string;
-    SESSION_SECRET: string;
-    SESSION_NAME: string;
-    SESSION_IDLE_TIMEOUT: string;
-    SESSION_ABSOLUTE_TIMEOUT: string;
+    NODE_ENV: string
+    APP_PORT: string
+    APP_HOSTNAME: string
+    APP_PROTOCOL: string
+    APP_SECRET: string
+    MONGO_USERNAME: string
+    MONGO_PASSWORD: string
+    MONGO_HOST: string
+    MONGO_PORT: string
+    MONGO_DATABASE: string
+    REDIS_PORT: string
+    REDIS_HOST: string
+    REDIS_PASSWORD: string
+    SESSION_SECRET: string
+    SESSION_NAME: string
+    SESSION_IDLE_TIMEOUT: string
+    SESSION_ABSOLUTE_TIMEOUT: string
+    GOOGLE_OAUTH_CLIENT_ID: string
+    GOOGLE_OAUTH_CLIENT_SECRET: string
+    GITHUB_OAUTH_CLIENT_ID: string
+    GITHUB_OAUTH_CLIENT_SECRET: string
   }
 }
