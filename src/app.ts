67b8b1954eae6f8c2707ab1b43a738474c55bb43
romanmunar <roman.munar1@gmail.com>
2021-04-09 12:30:03 +0800

    updated app

--- src/app.ts
+++ src/app.ts
@@ -1,27 +1,31 @@
 import express from 'express'
 import session, { Store } from 'express-session'
-import { SESSION_OPTIONS } from './config'
-import { home, login, register } from './routes' // verify, reset
+import { SESSION_OPTIONS, STATIC_DIR } from './config'
+import { home, upload, verify, reset } from './routes' // verify, reset
 import { notFound, serverError, active } from './middleware'
+import { startPassport } from './passport'
+require('dotenv').config()
 
 export const createApp = (store: Store) => {
   const app = express()
+
+  app.use(express.static(STATIC_DIR))
 
   app.use(express.json())
 
   app.use(session({ ...SESSION_OPTIONS, store }))
 
-  app.use(active)
+  startPassport(app)
+
+  app.use(upload)
+
+  // app.use(active) Let's make req.user be an object { userId, createdAt} So we can expire it after 3-5 days of inactivity
 
   app.use(home)
 
-  app.use(login)
+  app.use(verify)
 
-  app.use(register)
-
-  // app.use(verify)
-
-  // app.use(reset)
+  app.use(reset)
 
   app.use(notFound)
 
