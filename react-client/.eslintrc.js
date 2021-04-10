ed3921b2c368597cad7496bedc5d15845a8ccf11
romanmunar <roman.munar1@gmail.com>
2021-04-10 21:10:03 +0800

    added tailwindcss

---
+++ react-client/.eslintrc.js
@@ -0,0 +1,14 @@
+module.exports = {
+  parser: '@typescript-eslint/parser',
+  extends: ['plugin:@typescript-eslint/recommended'],
+  env: {
+    es6: true,
+    node: true,
+  },
+  rules: {
+    '@typescript-eslint/no-unused-vars': 'off',
+    '@typescript-eslint/explicit-function-return-type': 'off',
+    '@typescript-eslint/no-explicit-any': 'off',
+    '@typescript-eslint/explicit-module-boundary-types': 'off',
+  },
+}
