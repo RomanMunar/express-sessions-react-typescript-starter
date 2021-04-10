00c4ce02da1d519369cbea001ca218042d1b7b4f
romanmunar <roman.munar1@gmail.com>
2021-04-10 21:08:17 +0800

    allowed no-empty-interface rule on single extends

--- api/.eslintrc.js
+++ api/.eslintrc.js
@@ -10,5 +10,6 @@ module.exports = {
     '@typescript-eslint/explicit-function-return-type': 'off',
     '@typescript-eslint/no-explicit-any': 'off',
     '@typescript-eslint/explicit-module-boundary-types': 'off',
+    '@typescript-eslint/no-empty-interface': { allowSingleExtends: true },
   },
 }
